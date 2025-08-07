d3f8719aeb7e5f58b88b0a02305b5e61316c062b
Clester Ricky Jackson <jackson102890@gmail.com>
2025-08-07 11:10:12 -0600

    Complete Exercise 2

--- exercises/e2.js
+++ exercises/e2.js
@@ -5,7 +5,12 @@ import { data } from "../data/data";
 // Return example: ['name1', 'name2', ... , 'nameN']
 
 export function getAsteroidNames(data) {
-  // Your code goes here...
+  let astName = [];
+  const asteroids = data.asteroids;
+  asteroids.map((ast) => {
+    astName.push(ast.name);
+  })
+  return astName;
 }
 
 
